dc9dfc53346315d445ebbc7648265db8f8c3eb64
Helden de Siqueira <siqueirahelden@gmail.com>
2022-01-14 13:39:38 -0300

    Desafios 2, 3 e 4
    Desafios realizados com vue 2

--- desafio-02/app.js
+++ desafio-02/app.js
@@ -2,5 +2,13 @@ new Vue({
     el: '#desafio',
     data: {
         valor: ''
+    },
+    methods: {
+        exibirAlerta() {
+            alert('Estou alertando agora!')
+        },
+        alterarValor(event) {
+            this.valor = event.target.value
+        }
     }
 })
