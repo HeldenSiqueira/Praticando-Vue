e8fb867e3d5cdf07e1ada90e9697917216d46a7d
Helden de Siqueira <siqueirahelden@gmail.com>
2022-01-25 18:43:48 -0300

    seção 7

---
+++ seção 7 - Componentes/componente-into/src/Contador.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="contador">
+        <span>{{ contador }}</span>
+        <button @click="adicionar">+</button>
+         <button @click="subtrair">-</button>
+
+</template>
+
+<script>
+export default {
+  data() {
+    return{
+        contador: 0
+    }
+  },
+  methods: {
+      adicionar() {
+          this.contador++
+      },
+      subtrair() {
+          this.contador--
+      }
+  }
+}
+</script>
+
+<style>
+    .contador span {
+        border-bottom: 1px;
+    }
+</style>
