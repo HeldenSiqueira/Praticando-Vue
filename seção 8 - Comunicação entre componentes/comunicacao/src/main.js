9ec1829c5da74ac5640b4603bee14731c97e2b19
Helden de Siqueira <siqueirahelden@gmail.com>
2022-01-27 19:02:45 -0300

    Seção 8
    Iniciando Comunicação entre componentes

--- seção 8 - Comunicação entre componentes/comunicacao/src/main.js
+++ seção 8 - Comunicação entre componentes/comunicacao/src/main.js
@@ -1,9 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
-import Contador from './Contador.vue'
 
 Vue.config.productionTip = false
-Vue.component('app-contador')
 
 new Vue({
   render: h => h(App),
