c04cee61950550d2d6ab79a1d19c1bc1ba11f4d4
Helden de Siqueira <siqueirahelden@gmail.com>
2022-03-16 15:27:21 -0300

    revisão

--- Exercicios/desafio-04/app.js
+++ Exercicios/desafio-04/app.js
@@ -1,11 +1,16 @@
 new Vue({
 	el: '#desafio',
 	data: {
+		// criando atributo classe1
+		classe1:'destaque'
 
 	},
 	methods: {
 		iniciarEfeito() {
-
+			setInterval(() => {
+			this.classe1 = this.classe1 == 'destaque'
+			?'encolher' : 'destque'
+			}, 1000)
 		},
 		iniciarProgresso() {
 
