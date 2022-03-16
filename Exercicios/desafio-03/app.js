413d8069681bb697573573a33d7f7555c77c124a
Helden de Siqueira <siqueirahelden@gmail.com>
2022-03-16 15:21:01 -0300

    Revisão desafio 4

--- Exercicios/desafio-03/app.js
+++ Exercicios/desafio-03/app.js
@@ -2,5 +2,20 @@ new Vue({
     el: '#desafio',
     data: {
         valor: 0
+    },
+
+    //Propriedade computada
+    computed: {
+        resultado() {
+            return this.valor == 37 ? 'Valo Igual' : 'Valor Diferente'
+        }
+    },
+    //monitorando ações de determinada propriedade com Watch
+    watch: {
+        resultado() {
+            setTimeout(() => {
+                this.valor = 0
+            }, 5000)
+        }
     }
 });
