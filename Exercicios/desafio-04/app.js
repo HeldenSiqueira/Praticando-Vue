2893eb5704a2d11efaed85c474eba7818cc5c23a
Helden de Siqueira <siqueirahelden@gmail.com>
2022-03-17 14:34:51 -0300

    finalizando revisão desafio 4

--- Exercicios/desafio-04/app.js
+++ Exercicios/desafio-04/app.js
@@ -1,19 +1,45 @@
 new Vue({
 	el: '#desafio',
 	data: {
-		// criando atributo classe1
-		classe1:'destaque'
+		// criando atributo
+		classe1: 'destaque',
+		perigo: true,
+		classe3: '',
+		classe4: '',
+		cor5: '',
+		estilo5: {
+			width: '100px',
+			height: '100px',
+		},
+		width: '0',
+
 
 	},
 	methods: {
 		iniciarEfeito() {
+			//setInterval(()) Intervalo
 			setInterval(() => {
-			this.classe1 = this.classe1 == 'destaque'
-			?'encolher' : 'destque'
+				//se this.classe1 for igual a destaque vai receber incolher se não recebe destaque
+				this.classe1 = this.classe1 == 'destaque'
+					? 'encolher' : 'destaque'
 			}, 1000)
 		},
 		iniciarProgresso() {
+			let valor = 0
+			const temporizador = setInterval(() => {
+				valor += 5
+				this.width = '${ valor }%'
+				if (valor == 100) clearInterval(temporizador)
 
+			}, 500)
+		},
+
+		setPerigo(event) {
+			if (event.target.value == "true") {
+				this.perigo = true
+			} else if (event.target.value == "false") {
+				this.perigo = false
+			}
 		}
 	}
 })
