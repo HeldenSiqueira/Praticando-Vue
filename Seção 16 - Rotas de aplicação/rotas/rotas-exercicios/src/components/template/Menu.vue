56fb29a5ee34d91fb8b6eb756e927af299342a96
Helden de Siqueira <siqueirahelden@gmail.com>
2022-04-10 21:37:18 -0300

    Update Menu.vue

--- Seção 16 - Rotas de aplicação/rotas/rotas-exercicios/src/components/template/Menu.vue
+++ Seção 16 - Rotas de aplicação/rotas/rotas-exercicios/src/components/template/Menu.vue
@@ -2,9 +2,9 @@
     <nav class="menu">
         <ul>
             <router-link to="/" tag="li" active-class="active" exact>
-                <a>InícioO</a></router-link>
+                <a>Início</a></router-link>
             <router-link to="/usuario" tag="li" active-class="active">
-                <a>Usuário</a></router-link>
+                <a>UsuáriOo</a></router-link>
         </ul>
     </nav>
 </template>
