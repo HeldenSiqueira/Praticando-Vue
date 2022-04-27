1f6439bc49490e0861079bf667a732d34250d9a6
Helden de Siqueira <siqueirahelden@gmail.com>
2022-04-27 15:38:46 -0300

    Update MenuAlt.vue

--- Seção 16 - Rotas de aplicação/rotas/rotas-exercicios/src/components/template/MenuAlt.vue
+++ Seção 16 - Rotas de aplicação/rotas/rotas-exercicios/src/components/template/MenuAlt.vue
@@ -2,7 +2,7 @@
     <nav class="menu">
         <ul>
             <router-link to="/" tag="li" active-class="active" exact>
-                <a></a></router-link>
+                <a>Home</a></router-link>
             <router-link to="/usuario" tag="li" active-class="active">
                 <a>User</a></router-link>
         </ul>
